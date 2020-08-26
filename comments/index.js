b4406857480b235afcbca2612c13d56634797693
unknown <sven.lecann@live.fr>
2020-08-26 17:03:59 +0200

    Section 2 - 30

--- comments/index.js
+++ comments/index.js
@@ -36,6 +36,12 @@ app.post('/posts/:id/comments', (req, res) => {
   res.status(201).send(comments);
 });
 
+app.post('/event', (req,res) => {
+  console.log('Event Received', req.body.type);
+
+  res.send({});
+})
+
 app.listen(4001, () => {
   console.log('Listening on 4001');
 });
