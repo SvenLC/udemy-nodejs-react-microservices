4ebdc95047e2f02a07cbba8854e731025345353e
Sven Le Cann <sven.lecann@live.fr>
2020-08-29 20:59:24 +0200

    Section 4 - 74

--- events-bus/index.js
+++ events-bus/index.js
@@ -11,10 +11,10 @@ app.post('/events', (req, res) => {
   const event = req.body;
   events.push(event);
   try {
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://posts-clusterip-srv:4000/events', event);
+    // axios.post('http://localhost:4001/events', event);
+    // axios.post('http://localhost:4002/events', event);
+    // axios.post('http://localhost:4003/events', event);
   } catch (error) {
     console.log(error);
   }
